2526548e96e684b7af2be326a2241d34d917ce4d
Christian Villeneuve <cvillene@c4r2p1.42nice.fr>
2025-11-05 08:40:59 +0100

    test

---
+++ revisions/ft_print_comb2/ft_print_comb2.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cvillene <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 06:54:39 by cvillene          #+#    #+#             */
+/*   Updated: 2025/07/31 07:08:30 by cvillene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	ft_putchar(n / 10 + '0');
+	ft_putchar(n % 10 + '0');
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putnbr(a);
+			ft_putchar(' ');
+			ft_putnbr(b);
+			if (a != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+	return (0);
+}
